7f7a3c1b38d33867f7f5999ae272b447888b055f
AdiL18 <adi.l@comstar.co.il>
2021-10-06 09:47:50 +0300

    webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,14 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const CompressionPlugin = require("compression-webpack-plugin");
 const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = (env, argv) => {
   return merge(common(env, argv), {
     mode: 'production',
     plugins: [
-      new webpack.DefinePlugin({ //<--key to reduce React's size
-        'process.env': {
-          'NODE_ENV': JSON.stringify('production')
-        }
-      }),
-      new webpack.optimize.UglifyJsPlugin(),
       new webpack.optimize.AggressiveMergingPlugin(),
+      new UglifyJsPlugin()
     ],
   })
 };
